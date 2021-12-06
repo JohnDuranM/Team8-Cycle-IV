9caf83d09f045d07029a462b27fc70dd7aaefa45
JohnDuranM <91157723+JohnDuranM@users.noreply.github.com>
2021-12-06 07:33:20 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Contact from './components/Contact';
+import Login from './components/Login';
+
+const App =()=>{
+
+    return(
+        <>
+            {/* <Contact></Contact> */}
+            <Login></Login> 
+        </>
+    )
+}
+
+export default App;
+
+
